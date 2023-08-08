da4830eefaa75151e35a76c347273eb616997d58
goku <goku@gokus-MacBook-Pro.local>
2023-08-09 00:49:45 +0530

    credits & subscription details

--- controllers/getUserDetails.js
+++ controllers/getUserDetails.js
@@ -4,8 +4,8 @@ const getUserDetails = async (req, res, next) => {
     try {
         const userObj = await User.findOne({ email: req.email });
         if (userObj) {
-            const { email, name, plan, limit, current_usage, renewalDate } = userObj;
-            return res.status(200).json({ email, name, plan, limit, current_usage, renewalDate });
+            const { email, name, plan, limit, current_usage, license_key } = userObj;
+            return res.status(200).json({ email, name, plan, limit, current_usage, license_key });
         } else {
             return res.status(404).json({ message: "User not found" });
         }
