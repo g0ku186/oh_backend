c3b4ab455ca4970645b1fb7b9b6180158c59edf1
goku <goku@gokus-MacBook-Pro.local>
2023-08-09 03:46:15 +0530

    added FormData package

--- controllers/uploadToCF.js
+++ controllers/uploadToCF.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+const FormData = require('form-data');
 const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
 const uploadToCF = async (url, retries = 3, delay = 3000) => {
@@ -9,7 +10,7 @@ const uploadToCF = async (url, retries = 3, delay = 3000) => {
         const res = await axios.post(
             `https://api.cloudflare.com/client/v4/accounts/${process.env.CF_accountId}/images/v1`, body,
             {
-                headers: { "Authorization": `Bearer ${process.env.CF_apiKey}` },
+                headers: { "Authorization": `Bearer ${process.env.CF_apiKey}`, ...body.getHeaders() }, // Include form boundary
             }
         );
         return res.data;
