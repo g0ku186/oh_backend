95a091b688b82298b9bcffdf5f23618fbf6d53c7
goku <goku@gokus-MacBook-Pro.local>
2023-08-25 19:52:20 +0530

    commented deleting from CF

--- controllers/image/deleteImg.js
+++ controllers/image/deleteImg.js
@@ -11,19 +11,19 @@ const deleteImg = async (req, res, next) => {
         if (!image) {
             return res.status(404).json({ message: 'Image not found' });
         }
-        const cf_id = image.cf_id;
-        let upscale_cf_id = null;
-        if (image.upscaled) {
-            upscale_cf_id = image.upscale_cf_id;
-        }
+        // const cf_id = image.cf_id;
+        // let upscale_cf_id = null;
+        // if (image.upscaled) {
+        //     upscale_cf_id = image.upscale_cf_id;
+        // }
 
         await Generations.updateOne({ imgId, email }, { isDeleted: true });
-        if (cf_id) {
-            const deleteFromCFResponse = await deleteFromCF(cf_id);
-        }
-        if (upscale_cf_id) {
-            await deleteFromCF(upscale_cf_id);
-        }
+        // if (cf_id) {
+        //     const deleteFromCFResponse = await deleteFromCF(cf_id);
+        // }
+        // if (upscale_cf_id) {
+        //     await deleteFromCF(upscale_cf_id);
+        // }
         return res.status(200).json({ message: 'Image deleted successfully' });
     } catch (err) {
         console.log("=============ERROR: Image Delete Error=============");
